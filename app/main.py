0d617149587d706aa51470fa7b6d662f82f17890
Ishanphogat17 <Ishanphogat17@gmail.com>
2025-11-27 00:19:05 +0530

    added echo command

--- app/main.py
+++ app/main.py
@@ -8,7 +8,11 @@ def main():
         command = input()
         if command == "exit":
             break
-        print(f"{command}: command not found")
+        if command.startswith('echo '):
+            message = command[5:]
+            print(message)
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
