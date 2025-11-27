91499795c6169dabe6d828bc98278b8176196882
Ishanphogat17 <Ishanphogat17@gmail.com>
2025-11-27 11:17:27 +0530

    added type command

--- app/main.py
+++ app/main.py
@@ -14,5 +14,14 @@ def main():
         else:
             print(f"{command}: command not found")
 
+        if command.startswith('type '):
+            message = command[5:]
+            if message == "exit" or message == "type" or message == "echo":
+                print(f"{message} is a shell builtin")
+            else:
+                print(f"{message}: not found")
+        else:
+            print(f"{command}: not found")
+
 if __name__ == "__main__":
     main()
